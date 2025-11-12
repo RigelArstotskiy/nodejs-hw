856f9c6fbe8fc0ae5078f53b1f98d0978234519e
Mykyta <nik.from.varga@gmail.com>
2025-11-12 10:23:52 +0200

    create routes, i dependencies and errors

---
+++ src/server.js
@@ -0,0 +1,59 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
+const app = express();
+const PORT = 3000;
+
+//CLASSIC MIDDLEWARE
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+//ROUTES
+app.get('/notes', (req, res) => {
+  res.status(200).json([{ message: 'Retrieved all notes' }]);
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Rertieved note with ID: ${noteId}` });
+});
+
+//ERROR MIDDLEWARE
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+}); //this should stay before 404, so I can simulate server error
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({
+    message: 'Internal Server Error',
+    error: err.message,
+  });
+});
+
+//START SERVER
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
