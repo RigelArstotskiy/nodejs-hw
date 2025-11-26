4863d6af0309624bd8b25d5961ff99cb19584724
Mykyta Varha <rigelfrmarstotska@gmail.com>
2025-11-26 16:27:35 +0200

    deleted ! in errorHandler before isHttpError - that should fix inverse problem

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -3,7 +3,7 @@ import { isHttpError } from 'http-errors';
 export const errorHandler = (err, req, res, next) => {
   const isProd = process.env.NODE_ENV === 'production';
 
-  if (!isHttpError(err)) {
+  if (isHttpError(err)) {
     return res.status(err.status).json({
       message: isProd
         ? 'Something went wrong. Please try again later.'
