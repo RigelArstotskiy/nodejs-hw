e2b1d7deb9555d6eb3f3d455386835a5d1a6ef85
Mykyta Varha <rigelfrmarstotska@gmail.com>
2025-12-18 22:44:36 +0200

    forgot to add logic into userController - fixed this problem

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,19 @@
 // src/controllers/userController.js
 import createHttpError from 'http-errors';
+import { User } from '../models/user.js';
+import saveFileToCloudinary from '../utils/saveFileToCloudinary.js';
 
-export const updateUserAvatar = async (req, res, next) => {
+export const updateUserAvatar = async (req, res) => {
   if (!req.file) {
-    next(createHttpError(400, 'No file'));
-    return;
+    throw createHttpError(400, 'No file');
   }
-  res.status(200).json({ url: '' });
+
+  const result = await saveFileToCloudinary(req.file.buffer, req.user._id);
+
+  const updateUser = await User.findByIdAndUpdate(
+    req.user._id,
+    { avatar: result.secure_url },
+    { new: true },
+  );
+  res.status(200).json({ url: updateUser.avatar });
 };
