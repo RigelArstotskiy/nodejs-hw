02cd20dd0502773595cf3c095bfed7cc9f3709ae
Mykyta Varha <rigelfrmarstotska@gmail.com>
2025-12-18 03:24:21 +0200

    changed imports with new file name (sendMail.js)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,5 @@
 // src/controllers/userController.js
 import createHttpError from 'http-errors';
-import { User } from '../models/user.js';
 
 export const updateUserAvatar = async (req, res, next) => {
   if (!req.file) {
